4eca6cf1234255b0bcc036cff368f103bd919c83
Your Name In Single Quotes <your email here in single quotes>
2019-10-03 01:38:46 +0300

    kk

--- CSS/7.js
+++ CSS/7.js
@@ -1,13 +1,21 @@
 var naseem = function() {
-    var gh;
-    var hima = prompt ('type down your status');
+    var gh = prompt ('type down your status')
+    var hima;
+   
+    while (gh !== 'good' && gh !== 'bad')
+       gh = prompt ('try good or bad')
+    
 if (hima == 'good') {
     gh = 'hello';
+  
+   
 } else if (hima == 'bad') {
     gh = 'bye';
 
+  
 }
 return gh;
 
 }
+
 
