8e109a0829000b69636e3c374024005eb43e602b
Your Name In Single Quotes <your email here in single quotes>
2019-10-02 23:05:50 +0300

    gg

---
+++ CSS/7.js
@@ -0,0 +1,14 @@
+var naseem = function() {
+    var gh;
+    var hima = prompt ('type down your status');
+if (hima = 'good') {
+    gh = 'hello';
+} else if (hima = 'bad') {
+    gh = 'bye';
+
+}
+return gh;
+
+}
+
+document.write( ' <h3>' +naseem+ ' </ h3> ');
